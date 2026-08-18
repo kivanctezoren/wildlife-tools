f594666ff92842b54c85d6733b0610abe82a7f64
Kıvanç Tezören <kivanc.tezoeren@uni-siegen.de>
2026-08-18 14:42:20 +0200

    More OOM and GPU-CPU control

--- wildlife_tools/similarity/cosine.py
+++ wildlife_tools/similarity/cosine.py
@@ -19,28 +19,47 @@ def cosine_similarity(a, b, memory_limit: int | None = None, dtype: torch.dtype 
     if a.ndim != 2 or b.ndim != 2:
         raise ValueError("Expected two 2D feature matrices.")
 
+    # FIXME: The occasional RAM OOM error arises from the final result matrix sometimes being too large to fit in CPU 
+    #   RAM. Check available memory and expected size of result matrix beforehand before computation (and before
+    #   eventually storing it in CPU RAM via `.cpu()`)
+
     if memory_limit is None:
         similarity = torch.matmul(F.normalize(a), F.normalize(b).T)
     else:
-        # Limit peak memory by computing in blocks
-        dtype_size = torch.tensor([], dtype=dtype).element_size()
-        block_size = memory_limit // max(1, b.shape[0] * dtype_size)
-        block_size = max(1, min(a.shape[0], block_size))  # Clamp to [1, row count of a]
+        try:
+            # Try computing the full similarity matrix at once on GPU
+            similarity = torch.empty((a.shape[0], b.shape[0]), dtype=dtype).to(device)
+            similarity = torch.matmul(F.normalize(a), F.normalize(b).T)
+        except torch.cuda.OutOfMemoryError:
+            # TODO log the OOM error
+            # Limit peak memory by computing in blocks, store result part by part in CPU memory
+            dtype_size = torch.tensor([], dtype=dtype).element_size()
+            block_size = memory_limit // max(1, b.shape[0] * dtype_size)
+            block_size = max(1, min(a.shape[0], block_size))  # Clamp to [1, row count of a]
 
-        a_norm = torch.linalg.vector_norm(a, dim=1, keepdim=True).clamp_min_(1e-12)
-        b_norm = torch.linalg.vector_norm(b, dim=1, keepdim=True).clamp_min_(1e-12)
-        b_normalized = b / b_norm
+            a_norm = torch.linalg.vector_norm(a, dim=1, keepdim=True).clamp_min_(1e-12)
+            b_norm = torch.linalg.vector_norm(b, dim=1, keepdim=True).clamp_min_(1e-12)
+            b_normalized = b / b_norm
 
-        similarity = torch.empty((a.shape[0], b.shape[0]), dtype=dtype)  # TODO allow result on gpu?
-        for start in tqdm(range(0, a.shape[0], block_size), desc="Computing cosine similarity"):
-            stop = min(start + block_size, a.shape[0])
-            a_block = a[start:stop] / a_norm[start:stop]
-            mul = torch.matmul(a_block, b_normalized.T)
-            if mul.device != similarity.device:
-                mul = mul.to(similarity.device)
-            similarity[start:stop] = mul
+            similarity = torch.empty((a.shape[0], b.shape[0]), dtype=dtype).to("cpu")
+            
+            for start in tqdm(range(0, a.shape[0], block_size), desc="Computing cosine similarity"):
+                stop = min(start + block_size, a.shape[0])
+                a_block = a[start:stop] / a_norm[start:stop]
+                try:
+                    mul = torch.matmul(a_block, b_normalized.T)
+                    if mul.device != similarity.device:  # If mul is not on CPU
+                        mul = mul.to(similarity.device)
+                except torch.cuda.OutOfMemoryError:
+                    # TODO: Log the OOM error
+                    # Still OOM error, fallback to CPU computation entirely
+                    device = torch.device("cpu")
+                    a_block = a_block.to(device)
+                    b_normalized = b_normalized.to(device)
+                    mul = torch.matmul(a_block, b_normalized.T)
+                similarity[start:stop] = mul
 
-    return similarity.numpy()
+    return similarity.cpu().numpy()
 
 
 class CosineSimilarity:
